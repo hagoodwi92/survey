99bc6824f07f4db861baf4d462e12b864b060094
Alex Goodwin <hagoodwi@gmail.com>
2020-08-12 16:30:23 -0700

    complete project
    Co-authored-by: Mark Tiapkov <mark.tiapkov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
   $("#form0ne").submit(function(event){
     const nameInput = $("input#name").val();
     const seasonInput = $("#season").val();
-    const foodInput = $("#food").val();
+    const foodInput = $("input:radio[name=food]:checked").val();
+    const bornInput = $("#born").val();
+    const favoriteColor = $("#color").val();
 
     $(".name").text(nameInput);
     $(".season").text(seasonInput);
-    $(".food").text(seasonInput);
+    $(".food").text(foodInput);
+    $(".born").text(bornInput);
+    $(".color").text(favoriteColor);
 
     $("#answers").show();
 
@@ -17,18 +21,3 @@ $(document).ready(function(){
 });
 
 
-
-
-// console.log("Script executing!");
-// $(document).ready(function() {
-//   $("#formOne").submit(function() {
-//     const nameInput = $("input#name").val();
-  
-//     $(".name").text(nameInput);
-
-
-
-//     $("#answers").show();
-//     event.preventDefault();
-//   });
-// });
